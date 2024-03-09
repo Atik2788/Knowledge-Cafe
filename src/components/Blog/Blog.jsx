29b83245a28357e6aeb9ba342b2db99b2ad594a5
Atik2788 <ashanto653.2@gmail.com>
2024-03-09 21:33:26 +0600

    second

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,31 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title, cover, reading_time, author_img, author, posted_date, hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button>Bookmark</button>
+                </div>
+            </div>
+
+            <h2 className='text-4xl mb-3'>{title} </h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key = {idx} className='ml-2 text-xl ;'> <a href='' className='text-red-300'>{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
@@ -14,3 +35,4 @@ Blog.propTypes = {
 }
 
 export default Blog;
+
